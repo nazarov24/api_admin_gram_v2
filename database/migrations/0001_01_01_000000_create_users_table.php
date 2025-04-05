7f236e89153a72f77057823f87288cd79bb60251
ShahbozQurbonov <shahbozq571@gmail.com>
2025-04-05 11:06:14 +0500

    m: user client performers employess

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name',255);
+            $table->string('last_name',255);
+            $table->string('patronymic',255);
+            $table->string('phone',255);
+            $table->string('second_phone',255);
+            $table->timestamps('birth_date');
+            $table->tinyInteger('gender')->default(0);
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
--- database/migrations/2025_03_10_033609_create_clients_table.php
+++ database/migrations/2025_03_10_033609_create_clients_table.php
@@ -15,29 +15,23 @@ class CreateClientsTable extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('phone', 255);
-            $table->string('first_name', 255)->nullable();
-            $table->string('last_name', 255)->nullable();
-            $table->string('patronymic', 255)->nullable();
-            $table->tinyInteger('gender')->nullable();
-            $table->timestamp('birth_date')->nullable();
-            $table->string('email', 255)->nullable();
-            $table->string('password', 255)->nullable();
-            $table->string('login', 255)->nullable();
-            $table->timestamp('created_at')->nullable();
-            $table->timestamp('updated_at')->nullable();
             $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('login', 255)->nullable();
+            $table->string('password', 255)->nullable();
+            $table->string('avatar', 255)->nullable();
+            $table->tinyInteger('status')->default(1);
             $table->text('fcm_token')->nullable();
             $table->tinyInteger('is_online')->default(0);
             $table->string('socket_id', 100)->nullable();
+            $table->double('rating')->default(0.98);
+            $table->timestamp('deleted_at')->nullable();
+            $table->string('device');
             $table->unsignedBigInteger('division_id')->nullable();
-            $table->string('id_num_passport', 255)->nullable();
-            $table->tinyInteger('is_active')->default(1);
             $table->text('dop_info')->nullable();
-            $table->string('avatar', 255)->nullable();
-            $table->unsignedBigInteger('app_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
 
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
         });
     }
 
